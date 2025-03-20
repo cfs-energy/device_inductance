417571707f84bbbc7d6306c750b9c9b2bd593381
James Logan <jlogan@cfs.energy>
2025-03-20 15:28:46 -0400

    more updates to make gmsh outputs more deterministic

--- device_inductance/mesh.py
+++ device_inductance/mesh.py
@@ -69,14 +69,18 @@ def _mesh_region(
 
     # Set up for nearly-deterministic meshing
     gmsh.option.setNumber("Mesh.RandomSeed", 12394871234)
-    gmsh.option.setNumber("Mesh.RandomFactor", 1e-9)
-    gmsh.option.setNumber("Mesh.RandomFactor3D", 1e-9)
+    #  Note this random factor is smaller than recommended, but
+    #  larger values result in behavior so non-repeatable that it causes
+    #  frequent unit test failures
+    gmsh.option.setNumber("Mesh.RandomFactor", 1e-18)
+    gmsh.option.setNumber("Mesh.RandomFactor3D", 1e-18)
+    gmsh.option.setNumber('General.NumThreads', 1)
 
     if mesh_mode == MeshMode.Triangular:
         gmsh.option.setNumber("Mesh.Algorithm", 8)
     elif mesh_mode == MeshMode.QuasiStructuredQuad:
         # we don't want to be left with any triangles! (see gmsh tutorial 11)
-        gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2)
+        gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 0)
         gmsh.option.setNumber("Mesh.RecombineAll", 1)
         gmsh.option.setNumber("Mesh.Algorithm", 11)
         gmsh.option.setNumber("Mesh.QuadqsSizemapMethod", 0)
