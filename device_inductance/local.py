ad335806b8296204c256521a48157458f01fce17
James Logan <jlogan@cfs.energy>
2025-07-02 13:05:37 -0400

    faster method for finding grid index

--- device_inductance/local.py
+++ device_inductance/local.py
@@ -310,8 +310,8 @@ def _allocate_current_regular(
     jtor_per_amp = np.zeros_like(meshes[0])  # [A-turns/m^2 / A]
     for r, z, n in zip(*fil_rzn):
         # Get indices of location of this filament
-        ri = np.argmin(np.abs(rgrid - r))
-        zi = np.argmin(np.abs(zgrid - z))
+        ri = int((r - np.min(rgrid)) / dr)
+        zi = int((z - np.min(zgrid)) / dz)
         # Set current density for that unit cell
         # so that the total for the cell comes out to the
         # correct total current
